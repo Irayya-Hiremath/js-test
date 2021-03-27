1cb0c6e77d4ab60f3ef8ac71071e1c1a29bd79b8
Irayya-Hiremath <ihiremath1991@gmail.com>
2021-03-27 20:28:17 +0530

    test completed

---
+++ ui-food.js
@@ -0,0 +1,17 @@
+
+function openNav() {
+  document.getElementById("nav").style.width = "60%" ;
+  document.getElementById("nav").style.border= "2px solid black";
+  document.getElementById("nav").style.height = "100%" ;
+  document.getElementById("nav").style.padding = "30px" ;
+
+
+}
+
+function closeNav() {
+  document.getElementById("nav").style.width = "0";
+  document.getElementById("nav").style.border= "0px";
+  document.getElementById("nav").style.padding = "0px" ;
+
+
+}
